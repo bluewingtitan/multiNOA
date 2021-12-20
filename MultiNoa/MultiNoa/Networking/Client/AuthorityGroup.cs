5d45a712b396d7a36a3f8afd92418b9a92aac5de
Dominik Mezler <thebluewingtitan@gmail.com>
2021-12-20 21:02:32 +0100

    small fixes to authority groups

--- MultiNoa/MultiNoa/Networking/Client/AuthorityGroup.cs
+++ MultiNoa/MultiNoa/Networking/Client/AuthorityGroup.cs
@@ -9,36 +9,36 @@ namespace MultiNoa.Networking.Client
         /// The base level everyone shares.
         /// Can't be revoked.
         /// </summary>
-        Default = 0,
+        Default = AuthorityGroupConst.Default,
         
         /// <summary>
         /// Ment for handling methods only game clients should be able execute
         /// </summary>
-        Client = 1,
+        Client = AuthorityGroupConst.Client,
         
         /// <summary>
         /// Ment for handling methods only other servers in the network should be able to execute
         /// </summary>
-        Server = 2,
+        Server = AuthorityGroupConst.Server,
         
         /// <summary>
         /// Ment for use in a scenario, where a single server/app controls others
         /// </summary>
-        ControlPlane = 3,
+        ControlPlane = AuthorityGroupConst.ControlPlane,
         
-        Custom0 = 4,
+        Custom0 = AuthorityGroupConst.Custom0,
         
-        Custom1 = 5,
+        Custom1 = AuthorityGroupConst.Custom1,
         
-        Custom2 = 6,
+        Custom2 = AuthorityGroupConst.Custom2,
         
-        Custom3 = 7,
+        Custom3 = AuthorityGroupConst.Custom3,
         
         
         /// <summary>
         /// Special level, that allows for invocation of ALL methods. THIS SHOULD NEVER BE GIVEN TO NORMAL PLAYERS!
         /// </summary>
-        Admin = 8,
+        Admin = AuthorityGroupConst.Admin,
     }
 
     public static class AuthorityGroupConst
